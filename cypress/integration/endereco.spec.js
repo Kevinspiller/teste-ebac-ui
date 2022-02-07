03df83211ef89a5070a39ff811b1b3b1e4b283a5
Kevin Spiller <kevinspillereazy@gmail.com>
2022-02-07 13:40:24 -0300

    Page objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     before(() => {
@@ -8,9 +9,8 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         })
     });
     
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-       
-        //pre-cadastro
-        
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Kelvin', 'Spiller', 'Nubank', 'Brasil', 'Av Nereu Ramos', '1231', 'São Paulo', 'São Paulo', '89801002', '1152321','email@email.com')
+        cy.get('.woocommerce-message').should('contain', "Endereço alterado com sucesso.")
     });
 });
