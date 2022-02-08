c17a23f1a211ead28da17ae6942f7bcdcb1dd613
Kevin Spiller <kevinspillereazy@gmail.com>
2022-02-07 21:13:42 -0300

    Massa de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     before(() => {
@@ -9,8 +10,25 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         })
     });
     
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
-        EnderecoPage.editarEnderecoFaturamento('Kelvin', 'Spiller', 'Nubank', 'Brasil', 'Av Nereu Ramos', '1231', 'São Paulo', 'São Paulo', '89801002', '1152321','email@email.com')
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Kevin', 'Spiller', 'Nubank', 'Brasil', 'Av Nereu Ramos', '1231', 'São Paulo', 'São Paulo', '89801002', '1152321','email@email.com')
+        cy.get('.woocommerce-message').should('contain', "Endereço alterado com sucesso.")
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+        )
         cy.get('.woocommerce-message').should('contain', "Endereço alterado com sucesso.")
     });
 });
